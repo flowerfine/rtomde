7a1eb49dbd64f71070bed25703d37d42ce123f9e
wangqi <1942460489@qq.com>
2020-12-31 23:33:57 +0800

    测试mybatis的sqlsession功能

--- rtomde-executor/src/main/java/cn/sliew/rtomde/executor/controller/UserController.java
+++ rtomde-executor/src/main/java/cn/sliew/rtomde/executor/controller/UserController.java
@@ -3,10 +3,14 @@ package cn.sliew.rtomde.executor.controller;
 import cn.sliew.rtomde.executor.mapper.SysUser;
 import cn.sliew.rtomde.executor.mapper.SysUserMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -14,9 +18,18 @@ public class UserController {
 
     @Autowired
     private SysUserMapper sysUserMapper;
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
 
     @GetMapping("/user/{id}")
     public SysUser getUser(@PathVariable Long id) {
         return sysUserMapper.selectByPrimaryKey(id);
+    }
+
+    @GetMapping("/sqluser/{id}")
+    public SysUser getSqlUser(@PathVariable Long id) {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<Object> objects = sqlSession.selectList("cn.sliew.rtomde.executor.mapper.SysUserMapper.selectByPrimaryKey", id);
+        return (SysUser) objects.get(0);
     }
 }
---
+++ rtomde-mybatis/src/main/java/cn/sliew/rtomde/mybatis/config/MybatisConfig.java
@@ -0,0 +1,8 @@
+package cn.sliew.rtomde.mybatis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisConfig {
+
+}
