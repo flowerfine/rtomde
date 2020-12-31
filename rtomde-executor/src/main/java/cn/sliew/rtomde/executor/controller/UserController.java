15150dc7345c9cdffd5c060ade74590bbcec4b68
wangqi <1942460489@qq.com>
2020-12-31 21:54:01 +0800

    测试mybatis功能

--- rtomde-executor/src/main/java/cn/sliew/rtomde/executor/controller/UserController.java
+++ rtomde-executor/src/main/java/cn/sliew/rtomde/executor/controller/UserController.java
@@ -2,10 +2,13 @@ package cn.sliew.rtomde.executor.controller;
 
 import cn.sliew.rtomde.executor.mapper.SysUser;
 import cn.sliew.rtomde.executor.mapper.SysUserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class UserController {
 
@@ -13,7 +16,7 @@ public class UserController {
     private SysUserMapper sysUserMapper;
 
     @GetMapping("/user/{id}")
-    public SysUser getUser(Long id) {
+    public SysUser getUser(@PathVariable Long id) {
         return sysUserMapper.selectByPrimaryKey(id);
     }
 }
