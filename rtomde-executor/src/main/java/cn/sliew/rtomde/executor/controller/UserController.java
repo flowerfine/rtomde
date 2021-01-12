b2404c3d69ffe73963bd838828dec6b7ef4586e5
wangqi <wangqi@xinc818.group>
2021-01-12 15:35:57 +0800

    feature: 移除自动的代码生成

--- rtomde-executor/src/main/java/cn/sliew/rtomde/executor/controller/UserController.java
+++ rtomde-executor/src/main/java/cn/sliew/rtomde/executor/controller/UserController.java
@@ -1,27 +1,40 @@
 package cn.sliew.rtomde.executor.controller;
 
-import cn.sliew.rtomde.executor.mapper.SysUser;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.beans.BeanMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Slf4j
 @RestController
 public class UserController {
 
+    private ObjectMapper mapper;
+
     @Autowired
     private SqlSessionFactory sqlSessionFactory;
 
+    @PostConstruct
+    public void init() {
+        mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
     @GetMapping("/sqluser/{id}")
-    public SysUser getSqlUser(@PathVariable Long id) {
+    public JsonNode getSqlUser(@PathVariable Long id) {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         List<Object> objects = sqlSession.selectList("selectByPrimaryKey", id);
-        return (SysUser) objects.get(0);
+        BeanMap beanMap = BeanMap.create(objects.get(0));
+        return mapper.valueToTree(beanMap);
     }
 }
