88463b5c13adf32139342a05f67379b4e286d6ad
wangqi <wangqi@xinc818.group>
2021-01-12 15:21:35 +0800

    feature: 添加dubbo的代码生成编译功能

---
+++ rtomde-executor/src/main/java/cn/sliew/rtomde/executor/compile/Compiler.java
@@ -0,0 +1,13 @@
+package cn.sliew.rtomde.executor.compile;
+
+public interface Compiler {
+
+    /**
+     * Compile java source code.
+     *
+     * @param code        Java source code
+     * @param classLoader classloader
+     * @return Compiled class
+     */
+    Class<?> compile(String code, ClassLoader classLoader);
+}
