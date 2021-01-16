42357ea09a9652329eb26d6aad0efd2a094d61e4
wangqi <1942460489@qq.com>
2021-01-16 11:14:07 +0800

    处理代码编译功能

---
+++ rtomde-common/src/main/java/cn/sliew/rtomde/common/utils/StringUtils.java
@@ -0,0 +1,31 @@
+package cn.sliew.rtomde.common.utils;
+
+public final class StringUtils {
+
+
+    /**
+     * is empty string.
+     *
+     * @param str source string.
+     * @return is empty.
+     */
+    public static boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+    /**
+     * is not empty string.
+     *
+     * @param str source string.
+     * @return is not empty.
+     */
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+
+
+
+    private StringUtils() {
+        throw new IllegalStateException("can't do this!");
+    }
+}
