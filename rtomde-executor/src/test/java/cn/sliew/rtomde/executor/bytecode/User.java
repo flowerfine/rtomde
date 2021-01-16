b6f9243d2f6bcd4751083c80fb0b8f841ba29c63
wangqi <1942460489@qq.com>
2021-01-16 12:49:25 +0800

    测试编译功能

---
+++ rtomde-executor/src/test/java/cn/sliew/rtomde/executor/bytecode/User.java
@@ -0,0 +1,20 @@
+package cn.sliew.rtomde.executor.bytecode;
+
+public class User {
+
+    private int age;
+    private String userName = "foo";
+    private String password;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ rtomde-executor/src/test/java/cn/sliew/rtomde/executor/bytecode/UserBuilder.java
@@ -0,0 +1,8 @@
+package cn.sliew.rtomde.executor.bytecode;
+
+public interface UserBuilder {
+
+    String getUserName(User user);
+
+    void setUserName(User user, String userName);
+}
