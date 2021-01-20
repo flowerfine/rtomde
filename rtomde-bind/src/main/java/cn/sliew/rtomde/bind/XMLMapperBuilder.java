d66c3c5d683f51b90c4def545d5d24b3ab77c9c5
wangqi <1942460489@qq.com>
2021-01-20 23:19:05 +0800

    feature: mapper xml parse

---
+++ rtomde-bind/src/main/java/cn/sliew/rtomde/bind/XMLMapperBuilder.java
@@ -0,0 +1,12 @@
+package cn.sliew.rtomde.bind;
+
+import java.util.Map;
+
+public class XMLMapperBuilder {
+
+    private final XPathParser parser;
+    private final Map<String, XNode> sqlFragments;
+    private final String resource;
+
+
+}
