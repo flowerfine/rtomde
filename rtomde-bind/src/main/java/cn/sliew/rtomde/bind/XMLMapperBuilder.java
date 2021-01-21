4d6903f0698409a744737042dd401411997c72c8
wangqi <wangqi@xinc818.group>
2021-01-21 16:01:31 +0800

    feature: xml parse

--- rtomde-bind/src/main/java/cn/sliew/rtomde/bind/XMLMapperBuilder.java
+++ rtomde-bind/src/main/java/cn/sliew/rtomde/bind/XMLMapperBuilder.java
@@ -3,13 +3,29 @@ package cn.sliew.rtomde.bind;
 import cn.sliew.rtomde.common.xml.XNode;
 import cn.sliew.rtomde.common.xml.XPathParser;
 
+import java.io.InputStream;
 import java.util.Map;
+import java.util.Properties;
 
 public class XMLMapperBuilder {
 
-//    private final XPathParser parser;
-//    private final Map<String, XNode> sqlFragments;
-//    private final String resource;
+    private final XPathParser parser;
+    private final Map<String, XNode> sqlFragments;
+    private final String resource;
+
+    public XMLMapperBuilder(InputStream inputStream, String resource, Map<String, XNode> fragments) {
+        this(new XPathParser(inputStream, true, new Properties(), new XMLMapperEntityResolver()),
+                resource,
+                fragments);
+    }
+
+    public XMLMapperBuilder(XPathParser parser, String resource, Map<String, XNode> fragments) {
+        this.parser = parser;
+        this.sqlFragments = fragments;
+        this.resource = resource;
+    }
+
+
 
 
 }
---
+++ rtomde-bind/src/test/java/cn/sliew/rtomde/bind/XMLMapperBuilderTest.java
@@ -0,0 +1,20 @@
+package cn.sliew.rtomde.bind;
+
+import cn.sliew.rtomde.common.resource.Resources;
+import cn.sliew.rtomde.common.xml.XPathParser;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.Collections;
+
+public class XMLMapperBuilderTest {
+
+    @Test
+    public void testParse() throws Exception {
+        InputStream inputStream = Resources.getResourceAsStream("PostMapper.xml");
+        XPathParser xPathParser = new XPathParser(inputStream);
+        XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(xPathParser, "PostMapper.xml", Collections.emptyMap());
+
+    }
+
+}
