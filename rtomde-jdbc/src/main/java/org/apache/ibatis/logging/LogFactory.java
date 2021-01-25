a39926500d76cc04f552539917c0f16d7bdd4b53
wangqi <wangqi@xinc818.group>
2021-01-25 15:27:55 +0800

    feature: log

---
+++ rtomde-jdbc/src/main/java/org/apache/ibatis/logging/LogFactory.java
@@ -0,0 +1,103 @@
+/**
+ *    Copyright 2009-2020 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.logging;
+
+import org.apache.ibatis.logging.log4j2.Log4j2Impl;
+import org.apache.ibatis.logging.nologging.NoLoggingImpl;
+import org.apache.ibatis.logging.slf4j.Slf4jImpl;
+import org.apache.ibatis.logging.stdout.StdOutImpl;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * @author Clinton Begin
+ * @author Eduardo Macarron
+ */
+public final class LogFactory {
+
+  /**
+   * Marker to be used by logging implementations that support markers.
+   */
+  public static final String MARKER = "MYBATIS";
+
+  private static Constructor<? extends Log> logConstructor;
+
+  static {
+    tryImplementation(LogFactory::useSlf4jLogging);
+    tryImplementation(LogFactory::useLog4J2Logging);
+    tryImplementation(LogFactory::useNoLogging);
+  }
+
+  private LogFactory() {
+    // disable construction
+  }
+
+  public static Log getLog(Class<?> clazz) {
+    return getLog(clazz.getName());
+  }
+
+  public static Log getLog(String logger) {
+    try {
+      return logConstructor.newInstance(logger);
+    } catch (Throwable t) {
+      throw new LogException("Error creating logger for logger " + logger + ".  Cause: " + t, t);
+    }
+  }
+
+  public static synchronized void useCustomLogging(Class<? extends Log> clazz) {
+    setImplementation(clazz);
+  }
+
+  public static synchronized void useSlf4jLogging() {
+    setImplementation(Slf4jImpl.class);
+  }
+
+  public static synchronized void useLog4J2Logging() {
+    setImplementation(Log4j2Impl.class);
+  }
+
+  public static synchronized void useStdOutLogging() {
+    setImplementation(StdOutImpl.class);
+  }
+
+  public static synchronized void useNoLogging() {
+    setImplementation(NoLoggingImpl.class);
+  }
+
+  private static void tryImplementation(Runnable runnable) {
+    if (logConstructor == null) {
+      try {
+        runnable.run();
+      } catch (Throwable t) {
+        // ignore
+      }
+    }
+  }
+
+  private static void setImplementation(Class<? extends Log> implClass) {
+    try {
+      Constructor<? extends Log> candidate = implClass.getConstructor(String.class);
+      Log log = candidate.newInstance(LogFactory.class.getName());
+      if (log.isDebugEnabled()) {
+        log.debug("Logging initialized using '" + implClass + "' adapter.");
+      }
+      logConstructor = candidate;
+    } catch (Throwable t) {
+      throw new LogException("Error setting Log implementation.  Cause: " + t, t);
+    }
+  }
+
+}
