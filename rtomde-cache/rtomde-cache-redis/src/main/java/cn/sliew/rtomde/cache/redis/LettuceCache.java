cb2d082972b6e50adce5cc8759a9d54c8dbab9bd
wangqi <wangqi@xinc818.group>
2021-03-18 15:41:55 +0800

    jdbc lettuce cache.

--- rtomde-cache/rtomde-cache-redis/src/main/java/cn/sliew/rtomde/cache/redis/LettuceCache.java
+++ rtomde-cache/rtomde-cache-redis/src/main/java/cn/sliew/rtomde/cache/redis/LettuceCache.java
@@ -5,11 +5,9 @@ import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
-import io.lettuce.core.codec.ByteArrayCodec;
 import org.apache.ibatis.cache.Cache;
 
 import java.time.Duration;
-import java.util.Map;
 import java.util.concurrent.locks.ReadWriteLock;
 
 public class LettuceCache implements Cache {
@@ -22,7 +20,10 @@ public class LettuceCache implements Cache {
 
     private static StatefulRedisConnection connection;
 
-    private Integer timeout;
+    /**
+     * cache expire time with millseconds.
+     */
+    private Integer expire;
 
     public LettuceCache(final String id) {
         if (id == null) {
@@ -37,11 +38,8 @@ public class LettuceCache implements Cache {
                 .withTimeout(Duration.ofSeconds(1L))
                 .build();
         client = RedisClient.create(redisURI);
-        connection = client.connect(ByteArrayCodec.INSTANCE);
-    }
-
-    private Object execute(LettuceConnectionCallback callback) {
-        return callback.doWithRedis(connection);
+        connection = client.connect(ProtostuffCodec.INSTANCE);
+        connection.setTimeout(Duration.ofSeconds(1L));
     }
 
     @Override
@@ -51,66 +49,35 @@ public class LettuceCache implements Cache {
 
     @Override
     public int getSize() {
-        return (Integer) execute(new LettuceConnectionCallback() {
-            @Override
-            public Object doWithRedis(StatefulRedisConnection connection) {
-                RedisCommands commands = connection.sync();
-                Map<byte[], byte[]> result = commands.hgetall(id.getBytes());
-                return result.size();
-            }
-        });
+        RedisCommands commands = connection.sync();
+        return commands.hlen(id).intValue();
     }
 
     @Override
     public void putObject(final Object key, final Object value) {
-        execute(new LettuceConnectionCallback() {
-            @Override
-            public Object doWithRedis(StatefulRedisConnection connection) {
-                RedisCommands commands = connection.sync();
-                final byte[] idBytes = id.getBytes();
-                commands.hset(idBytes, key.toString().getBytes(), value.toString().getBytes());
-
-                if (timeout != null && commands.ttl(idBytes) == -1) {
-                    commands.expire(idBytes, timeout);
-                }
-                return null;
-            }
-        });
+        RedisCommands commands = connection.sync();
+        commands.hset(id, key, value);
+        if (expire != null && commands.ttl(id) == -1) {
+            commands.expire(id, expire);
+        }
     }
 
     @Override
     public Object getObject(final Object key) {
-        return execute(new LettuceConnectionCallback() {
-            @Override
-            public Object doWithRedis(StatefulRedisConnection connection) {
-                RedisCommands commands = connection.sync();
-                return commands.hget(id.getBytes(), key.toString().getBytes());
-            }
-        });
+        RedisCommands commands = connection.sync();
+        return commands.hget(id, key);
     }
 
     @Override
     public Object removeObject(final Object key) {
-        return execute(new LettuceConnectionCallback() {
-            @Override
-            public Object doWithRedis(StatefulRedisConnection connection) {
-                RedisCommands commands = connection.sync();
-                return commands.hdel(id, key.toString());
-            }
-        });
+        RedisCommands commands = connection.sync();
+        return commands.hdel(id, key);
     }
 
     @Override
     public void clear() {
-        execute(new LettuceConnectionCallback() {
-            @Override
-            public Object doWithRedis(StatefulRedisConnection connection) {
-                RedisCommands commands = connection.sync();
-                commands.del(id);
-                return null;
-            }
-        });
-
+        RedisCommands commands = connection.sync();
+        commands.del(id);
     }
 
     @Override
@@ -123,7 +90,7 @@ public class LettuceCache implements Cache {
         return "Redis {" + id + "}";
     }
 
-    public void setTimeout(Integer timeout) {
-        this.timeout = timeout;
+    public void setExpire(Integer expire) {
+        this.expire = expire;
     }
 }
