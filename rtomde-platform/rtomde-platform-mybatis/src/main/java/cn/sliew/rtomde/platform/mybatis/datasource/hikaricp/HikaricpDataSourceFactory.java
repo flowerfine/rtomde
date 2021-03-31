09048d30bf883f230f20aa3b10cf473a9783e5a4
wangqi <1942460489@qq.com>
2021-03-31 08:57:17 +0800

    mybatis hikaricp

---
+++ rtomde-platform/rtomde-platform-mybatis/src/main/java/cn/sliew/rtomde/platform/mybatis/datasource/hikaricp/HikaricpDataSourceFactory.java
@@ -0,0 +1,50 @@
+package cn.sliew.rtomde.platform.mybatis.datasource.hikaricp;
+
+import cn.sliew.rtomde.platform.mybatis.datasource.DataSourceFactory;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigValue;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.util.Map;
+import java.util.Properties;
+
+public class HikaricpDataSourceFactory implements DataSourceFactory {
+
+    private Properties properties;
+    private Config config;
+
+    public HikaricpDataSourceFactory() {
+        this.config = ConfigFactory.load("cn/sliew/rtomde/platform/mybatis/datasource/hikaricp/hikaricp.properties");
+    }
+
+    @Override
+    public void setProperties(Properties props) {
+        this.properties = props;
+    }
+
+    @Override
+    public DataSource getDataSource() {
+        //no arguments constructor is lazy init datasource until first getConnection
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName(this.properties.getProperty("driver"));
+        dataSource.setJdbcUrl(this.properties.getProperty("url"));
+        dataSource.setUsername(this.properties.getProperty("username"));
+        dataSource.setPassword(this.properties.getProperty("password"));
+
+        dataSource.setIdleTimeout(this.config.getLong("idleTimeout"));
+        dataSource.setMinimumIdle(this.config.getInt("minimumIdle"));
+        dataSource.setMaximumPoolSize(this.config.getInt("maximumPoolSize"));
+
+        Config datasourceConfig = this.config.getConfig("dataSourceProperties");
+        for (Map.Entry<String, ConfigValue> entry : datasourceConfig.entrySet()) {
+            dataSource.addDataSourceProperty(entry.getKey(), entry.getValue().unwrapped());
+        }
+        String profileSQL = this.properties.getProperty("profileSQL");
+        if (profileSQL != null && !profileSQL.isEmpty()) {
+            dataSource.addDataSourceProperty("profileSQL", Boolean.parseBoolean(profileSQL));
+        }
+        return dataSource;
+    }
+}
