0701e7c50cb4b0e743fe8b2d70c24e64a9b11dec
wangqi <1942460489@qq.com>
2021-04-01 08:12:01 +0800

    opt: throwable message collect.

--- rtomde-service/rtomde-service-bytecode/src/main/java/cn/sliew/rtomde/service/bytecode/config/dispatcher/MapperDispatcher.java
+++ rtomde-service/rtomde-service-bytecode/src/main/java/cn/sliew/rtomde/service/bytecode/config/dispatcher/MapperDispatcher.java
@@ -1,9 +1,10 @@
 package cn.sliew.rtomde.service.bytecode.config.dispatcher;
 
+import cn.sliew.milky.common.exception.ThrowableCollector;
+import cn.sliew.milky.log.Logger;
+import cn.sliew.milky.log.LoggerFactory;
 import cn.sliew.rtomde.platform.mybatis.config.MybatisApplicationOptions;
 import cn.sliew.rtomde.platform.mybatis.config.MybatisPlatformOptions;
-import cn.sliew.rtomde.platform.mybatis.mapping.MappedStatement;
-import lombok.extern.slf4j.Slf4j;
 import cn.sliew.rtomde.platform.mybatis.binding.MapperInvoker;
 import cn.sliew.rtomde.platform.mybatis.binding.MapperMethod;
 import cn.sliew.rtomde.platform.mybatis.binding.PlainMapperInvoker;
@@ -15,14 +16,19 @@ import javax.annotation.PostConstruct;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
 
-@Slf4j
 @Component
 public class MapperDispatcher {
 
+    private static final Logger log = LoggerFactory.getLogger(MapperDispatcher.class);
+
     private final ConcurrentMap<String, MapperInvoker> map = new ConcurrentHashMap<>(4);
+
+    private ThrowableCollector.Factory throwableCollectorFactory = () -> new ThrowableCollector();
 
     @Autowired
     private SqlSessionFactory sqlSessionFactory;
@@ -49,6 +55,27 @@ public class MapperDispatcher {
      * todo application
      */
     public Object execute(String application, String id, Object... params) {
-        return map.get(id).invoke(sqlSessionFactory.openSession(application), id, params);
+        ThrowableCollector throwableCollector = throwableCollectorFactory.create();
+        CompletableFuture<Object> future = CompletableFuture.supplyAsync(() -> map.get(id).invoke(sqlSessionFactory.openSession(application), id, params));
+        throwableCollector.execute(() -> future.get());
+        if (throwableCollector.isEmpty()) {
+            try {
+                return future.get();
+            } catch (InterruptedException e) {
+                // should never happen
+                log.error(e.getMessage(), e);
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                // should never happen
+                log.error(e.getMessage(), e);
+                throw new RuntimeException(e);
+            }
+        } else {
+            Throwable throwable = throwableCollector.getThrowable();
+            log.error(throwable.getMessage(), throwable);
+            throw new RuntimeException(throwable);
+        }
     }
+
+
 }
