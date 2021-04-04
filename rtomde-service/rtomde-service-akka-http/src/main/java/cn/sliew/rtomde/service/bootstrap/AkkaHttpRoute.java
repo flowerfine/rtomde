ba2befb3780a8be4d9e9084f8695dde7e0f396c2
wangqi <1942460489@qq.com>
2021-04-04 17:11:21 +0800

    feature: akka http server bootstrap.

---
+++ rtomde-service/rtomde-service-akka-http/src/main/java/cn/sliew/rtomde/service/bootstrap/AkkaHttpRoute.java
@@ -0,0 +1,80 @@
+package cn.sliew.rtomde.service.bootstrap;
+
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.StatusCodes;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class AkkaHttpRoute extends AllDirectives {
+
+    private final Map<String, Map<JsonNode, JsonNode>> state = new ConcurrentHashMap<>();
+
+    Route fixedRoute() {
+        return complete(StatusCodes.OK, "hhh");
+    }
+
+    Route dynamicRoute() {
+        Route fixedRoute = post(() ->
+                pathSingleSlash(() ->
+                        entity(Jackson.unmarshaller(MockDefinition.class), mock -> {
+                            Map<JsonNode, JsonNode> mappings = new HashMap<>();
+                            int size = Math.min(mock.getRequests().size(), mock.getResponses().size());
+                            for (int i = 0; i < size; i++) {
+                                mappings.put(mock.getRequests().get(i), mock.getResponses().get(i));
+                            }
+                            state.put(mock.getPath(), mappings);
+                            return complete("ok");
+                        })
+                )
+        );
+
+        // dynamic routing based on current state
+        Route dynamicRoute = post(() ->
+                state.entrySet().stream().map(mock ->
+                        path(mock.getKey(), () ->
+                                entity(Jackson.unmarshaller(JsonNode.class), input ->
+                                        complete(StatusCodes.OK, mock.getValue().get(input), Jackson.marshaller())
+                                )
+                        )
+                ).reduce(reject(), Route::orElse)
+        );
+
+        return concat(fixedRoute, dynamicRoute);
+    }
+
+    private static class MockDefinition {
+        private final String path;
+        private final List<JsonNode> requests;
+        private final List<JsonNode> responses;
+
+        public MockDefinition(@JsonProperty("path") String path,
+                              @JsonProperty("requests") List<JsonNode> requests,
+                              @JsonProperty("responses") List<JsonNode> responses) {
+            this.path = path;
+            this.requests = requests;
+            this.responses = responses;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public List<JsonNode> getRequests() {
+            return requests;
+        }
+
+        public List<JsonNode> getResponses() {
+            return responses;
+        }
+    }
+
+}
---
+++ rtomde-service/rtomde-service-akka-http/src/main/java/cn/sliew/rtomde/service/bootstrap/HttpServerBootstrap.java
@@ -0,0 +1,35 @@
+package cn.sliew.rtomde.service.bootstrap;
+
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.http.javadsl.Http;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpServerBootstrap implements ApplicationRunner {
+
+    @Value("${spring.application.name:akka-http-server}")
+    private String application;
+
+    private ActorSystem<Void> system;
+
+    @Autowired
+    private AkkaHttpRoute route;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // boot up server using the route as defined below
+        system = ActorSystem.create(Behaviors.empty(), application);
+        system.whenTerminated().onComplete(new ActorSystemShutdownHook(system), system.executionContext());
+
+        final Http http = Http.get(system);
+        //In order to access all directives we need an instance where the routes are define.
+        http.newServerAt("localhost", 8080).bind(route.fixedRoute())
+                .whenComplete(new HttpServerBootstrapHook("http://localhost:8080/", system));
+    }
+
+}
