54a13e3a41e1a59326c0ec21b62d770d6e865617
wangqi <1942460489@qq.com>
2021-04-05 17:26:42 +0800

    feature: scala Application bootstrap class.

---
+++ rtomde-service/rtomde-service-akka-http/src/main/scala/cn/sliew/rtomde/service/Application.scala
@@ -0,0 +1,11 @@
+package cn.sliew.rtomde.service
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+object Application extends App {
+    SpringApplication.run(classOf[Application])
+}
